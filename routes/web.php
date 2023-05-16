73ec08895dd666683a3e7c46ceccf8fc943cde79
NicholasConti <conti94@live.it>
2023-05-16 17:43:29 +0200

    migration & seeder

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return redirect('admin');
+});
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashBoardController::class, 'index'])->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
